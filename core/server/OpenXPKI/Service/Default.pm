PhoenixB/openxpki

commit 29ad7cb2402f28b4610cf4e983fa86bd902a254f

Rework socket communication, closes #955
Merge code of init and run in Service::Default
Introduce explict "end of transmission" message to signal
socket closure / avoid exceptions on regular disconnect
Rework error and exception handling related to socket timing

core/server/OpenXPKI/Service/Default.pm
@@ -43,37 +43,6 @@ sub init {
     # no session left in context
     OpenXPKI::Server::Context::killsession();
 
-    # TODO - this should be handled by the run method after some cleanup
-    # do session init, PKI realm selection and authentication
-    while ($state_of{$ident} ne 'MAIN_LOOP') {
-        my $msg = $self->collect();
-        my $is_valid = $self->__is_valid_message({
-            MESSAGE => $msg,
-        });
-        if (! $is_valid) {
-            $self->__send_error({
-                ERROR => "I18N_OPENXPKI_SERVICE_DEFAULT_RUN_UNRECOGNIZED_SERVICE_MESSAGE",
-            });
-        }
-        else { # valid message received
-            my $result;
-            eval { # try to handle it
-                $result = $self->__handle_message({ MESSAGE => $msg });
-                # persist session unless it was killed (we assume someone saved it before)
-                CTX('session')->persist if OpenXPKI::Server::Context::hascontext('session');
-            };
-            if (my $exc = OpenXPKI::Exception->caught()) {
-                $self->__send_error({ EXCEPTION => $exc });
-            }
-            elsif ($EVAL_ERROR) {
-                $self->__send_error({ EXCEPTION => $EVAL_ERROR });
-            }
-            else { # if everything was fine, send the result to the client
-                $self->talk($result);
-            }
-        }
-    }
-
     return 1;
 }
 
@@ -82,7 +51,7 @@ sub __is_valid_message : PRIVATE {
     my $ident   = ident $self;
     my $arg_ref = shift;
     my $message = $arg_ref->{'MESSAGE'};
-    my $message_name = $message->{'SERVICE_MSG'};
+    my $message_name = $message->{'SERVICE_MSG'} // '';
 
     ##! 32: 'message_name: ' . $message_name
     ##! 32: 'state: ' . $state_of{$ident}
@@ -887,62 +856,62 @@ sub run
   MESSAGE:
     while (1) {
         my $msg;
-        eval {
+
+        try {
             $msg = $self->collect();
-        };
-        if (my $exc = OpenXPKI::Exception->caught()) {
-            if ($exc->message() =~ m{I18N_OPENXPKI_TRANSPORT.*CLOSED_CONNECTION}xms) {
-                # client closed socket
-                last MESSAGE;
-            } else {
+        } catch($err) {
+            if (my $exc = OpenXPKI::Exception->caught()) {
                 $exc->rethrow();
             }
-        } elsif ($EVAL_ERROR) {
-            OpenXPKI::Exception->throw (
-            message => "I18N_OPENXPKI_SERVICE_DEFAULT_RUN_READ_EXCEPTION",
-            params  => {
-                EVAL_ERROR => $EVAL_ERROR,
-            });
+            OpenXPKI::Exception::Socket->throw (
+                message => "unable to collect message",
+                params  => { error => $err }
+            );
         }
 
+        # todo - clarify when this happens?
         last MESSAGE unless defined $msg;
 
-        my $is_valid = $self->__is_valid_message({ MESSAGE => $msg });
-        if (! $is_valid) {
+        # Disconnect on #EOT# Marker
+        if ($msg eq '#EOT#') {
+            $self->talk();
+            $self->__change_state({ STATE => 'NEW' });
+            return 1;
+        }
+
+        # the message is not expected in the current session flow
+        if (!$self->__is_valid_message({ MESSAGE => $msg })) {
             CTX('log')->system->debug("Invalid message received from client: ".($msg->{SERVICE_MSG} // "(empty)"));
             $self->__send_error({
                 ERROR => "I18N_OPENXPKI_SERVICE_DEFAULT_RUN_UNRECOGNIZED_SERVICE_MESSAGE",
             });
+            next MESSAGE;
         }
-        else { # valid message received
-            my $result;
-            # we dont need a valid session when we are not in main loop state
-            if ($state_of{$ident} eq 'MAIN_LOOP' && ! CTX('session')->is_valid) {
-                # check whether we still have a valid session (someone
-                # might have logged out on a different forked server)
-                CTX('log')->system->debug("Can't process client message: session is not valid (login incomplete)");
-                $self->__send_error({
-                    ERROR => 'I18N_OPENXPKI_SERVICE_DEFAULT_RUN_SESSION_INVALID',
-                });
-            }
-            else {
-                # our session is just fine
-                eval { # try to handle it
-                    $result = $self->__handle_message({ MESSAGE => $msg });
-                    # persist session unless it was killed (we assume someone saved it before)
-                    CTX('session')->persist if OpenXPKI::Server::Context::hascontext('session');
-                };
-                if (my $exc = OpenXPKI::Exception->caught()) {
-                    $self->__send_error({ EXCEPTION => $exc, });
-                }
-                elsif ($EVAL_ERROR) {
-                    $self->__send_error({ EXCEPTION => $EVAL_ERROR, });
-                }
-                else { # if everything was fine, send the result to the client
-                    $self->talk($result);
-                }
-            }
+
+        # if we are in the MAIN_LOOP we expect a valid session
+        if ($state_of{$ident} eq 'MAIN_LOOP' && ! CTX('session')->is_valid) {
+            CTX('log')->system->debug("Can't process client message: session is not valid (login incomplete)");
+            $self->__send_error({
+                ERROR => 'I18N_OPENXPKI_SERVICE_DEFAULT_RUN_SESSION_INVALID',
+            });
+            next MESSAGE;
         }
+
+        # we are either in a valid session or in the session init phase
+        try {
+            my $result = $self->__handle_message({ MESSAGE => $msg });
+            # persist session unless it was killed (we assume someone saved it before)
+            CTX('session')->persist if OpenXPKI::Server::Context::hascontext('session');
+            $self->talk($result);
+        } catch($err) {
+            if (my $exc = OpenXPKI::Exception->caught()) {
+                $self->__send_error({ EXCEPTION => $exc, });
+            } else {
+                $self->__send_error({ ERROR => $err });
+            }
+            next MESSAGE;
+        }
+
     }
     return 1;
 }

core/server/OpenXPKI/Client.pm
@@ -391,6 +391,9 @@ sub close_connection ($self) {
         warn "got close_connection on already closed socket";
         return;
     }
+
+    $self->talk('#EOT#');
+
     shutdown($self->_socket, 2); # we have stopped using this socket
     # for whatever reasons shutdown does not free the handle, see #645
     close($self->_socket);

core/server/OpenXPKI/Server.pm
@@ -368,14 +368,6 @@ sub process_request {
     eval { $rc = do_process_request(@_) };
 
     if (my $exc = OpenXPKI::Exception->caught()) {
-        if ($exc->message() =~ m{ (?:
-            I18N_OPENXPKI_TRANSPORT.*CLOSED_CONNECTION
-            | I18N_OPENXPKI_SERVICE_COLLECT_TIMEOUT
-        ) }xms) {
-            # exit quietly
-            return 1;
-        }
-
         # other OpenXPKI exceptions
         $msg = $exc->full_message();
     } elsif ($EVAL_ERROR) {

core/server/OpenXPKI/Service.pm
@@ -35,13 +35,8 @@ sub talk {
         my $rc = $transport{$ident}->write(
             $serialization{$ident}->serialize($arg)
         );
+        $self->__check_sigterm();
         $self->set_communication_state('can_receive');
-        if ($OpenXPKI::Server::stop_soon) {
-            ##! 1: 'stop_soon hit'
-            CTX('log')->system()->info("Child $$ terminated by SIGTERM");
-            CTX('config')->cleanup();
-            exit 0;
-        }
         return $rc;
     }
     else {
@@ -55,18 +50,12 @@ sub talk {
 }
 
 
-
 # get server response
 sub collect {
     my $self  = shift;
     my $ident = ident $self;
 
-    if ($OpenXPKI::Server::stop_soon) {
-        ##! 1: 'stop_soon hit'
-        CTX('log')->system()->info("Child $$ terminated by SIGTERM");
-        CTX('config')->cleanup();
-        exit 0;
-    }
+    $self->__check_sigterm();
 
     my $communication_state = $self->get_communication_state();
     ##! 2: "communication state: $communication_state"
@@ -96,19 +85,28 @@ sub collect {
         ##! 1: "ERROR: " . Dumper($error)
         $self->set_communication_state('can_send');
         if ($error eq "alarm\n") {
-            OpenXPKI::Exception->throw(
-                message => "I18N_OPENXPKI_SERVICE_COLLECT_TIMEOUT",
-                log => undef, # do not log this exception
-            );
+            return;
         }
-        # FIXME
-        die $error;
+        OpenXPKI::Exception::Socket->throw(
+            message => "socket timeout on collect",
+            params => { error => $error }
+        );
     }
     $self->set_communication_state('can_send');
     ##! 128: 'collect: ' . Dumper $result
     return $result;
 }
 
+# check if this process has recevied a stop signal
+sub __check_sigterm {
+
+    return unless ($OpenXPKI::Server::stop_soon);
+    ##! 1: 'stop_soon hit'
+    CTX('log')->system()->info("Child $$ terminated by SIGTERM");
+    CTX('config')->cleanup();
+    exit 0;
+
+}
 
 sub __get_error {
     my $self  = shift;

core/server/OpenXPKI/Service/CLI.pm
@@ -102,6 +102,12 @@ sub run {
 
         last MESSAGE unless defined $msg;
 
+        # Disconnect on #EOT# Marker
+        if ($msg eq '#EOT#') {
+            $self->talk();
+            return 1;
+        }
+
         # messages are JSON Web Tokens so we need to unwrap them
         ##! 16: $msg
 
@@ -323,9 +329,8 @@ sub collect {
         alarm 0;
         ##! 1: "ERROR: " . Dumper($error)
         if ($error eq "alarm\n") {
-            OpenXPKI::Exception->throw(
-                message => "I18N_OPENXPKI_SERVICE_COLLECT_TIMEOUT",
-                log => undef, # do not log this exception
+            OpenXPKI::Exception::Timeout->throw(
+                message => "socket timeout on collect",
             );
         }
         # FIXME

commit 3ba674c728a5e74f0243dfa6e363822ea23d9cc9

Fix error handling regression bug introduced in 29ad7cb2
This resulted in "FATAL Unable to serialize" when the server tried to serialize/send whole exception objects to the client.

core/server/OpenXPKI/Service/Default.pm
@@ -904,8 +904,8 @@ sub run
             CTX('session')->persist if OpenXPKI::Server::Context::hascontext('session');
             $self->talk($result);
         } catch($err) {
-            if (my $exc = OpenXPKI::Exception->caught()) {
-                $self->__send_error({ EXCEPTION => $exc, });
+            if ($err->isa('OpenXPKI::Exception')) {
+                $self->__send_error({ EXCEPTION => $err });
             } else {
                 $self->__send_error({ ERROR => $err });
             }